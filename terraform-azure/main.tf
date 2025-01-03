d47c838f4dd47b5e308fd4e7bd596e7ba35f7c5c
rubyfeller <ruby.feller@student.fontys.nl>
2025-01-03 15:36:33 +0100

    Monitoring additions and improvements

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -3,6 +3,71 @@ resource "azurerm_resource_group" "openremote-rg" {
   location = var.region
 }
 
+resource "azurerm_log_analytics_workspace" "openremote-law" {
+  name                = "openremote-law"
+  location            = azurerm_resource_group.openremote-rg.location
+  resource_group_name = azurerm_resource_group.openremote-rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_data_collection_endpoint" "openremote-dce" {
+  name                          = "openremote-dce"
+  resource_group_name           = azurerm_resource_group.openremote-rg.name
+  location                      = azurerm_resource_group.openremote-rg.location
+  kind                          = "Linux"
+  public_network_access_enabled = true
+  description                   = "monitor_data_collection_endpoint"
+}
+
+resource "azurerm_monitor_data_collection_rule" "openremote-dcr-vm-metrics" {
+  name                        = "openremote-dcr-vm-metrics"
+  resource_group_name         = azurerm_resource_group.openremote-rg.name
+  location                    = azurerm_resource_group.openremote-rg.location
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.openremote-dce.id
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.openremote-law.id
+      name                  = "vm-metrics-destination"
+    }
+  }
+
+  data_flow {
+    streams = ["Microsoft-Perf"]
+    destinations = ["vm-metrics-destination"]
+  }
+  
+  data_sources {
+    performance_counter {
+      name = "vm-metrics"
+      streams = ["Microsoft-Perf"]
+      counter_specifiers = [
+        "\\Processor(_Total)\\% Processor Time",
+        "\\Memory\\Available MBytes",
+        "\\LogicalDisk(_Total)\\% Free Space",
+        "\\LogicalDisk(_Total)\\Free Megabytes"
+      ]
+      sampling_frequency_in_seconds = 60
+    }
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "azure_monitor" {
+  name                       = "AzureMonitorLinuxAgent"
+  virtual_machine_id         = azurerm_linux_virtual_machine.openremote-vm.id
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorLinuxAgent"
+  type_handler_version       = "1.9"
+  auto_upgrade_minor_version = true
+}
+
+resource "azurerm_monitor_data_collection_rule_association" "vm_metrics" {
+  name                    = "vm-metrics-dcra"
+  target_resource_id      = azurerm_linux_virtual_machine.openremote-vm.id
+  data_collection_rule_id = azurerm_monitor_data_collection_rule.openremote-dcr-vm-metrics.id
+}
+
 resource "azurerm_virtual_network" "openremote-vn" {
   name                = "openremote-network"
   resource_group_name = azurerm_resource_group.openremote-rg.name
